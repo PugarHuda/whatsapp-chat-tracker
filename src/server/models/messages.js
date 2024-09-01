7fdef709d4a24909e384eb80657b3e7f5f20c39a
Pugar Huda Mantoro <hudapugar@gmail.com>
2024-09-01 17:24:33 +0700

    update: fix error

--- src/server/models/messages.js
+++ src/server/models/messages.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import { MongoClient } from "mongodb";
 
-const MessageSchema = new mongoose.Schema({
-  from: String,
-  body: String,
-  timestamp: Date,
-});
+// const MessageSchema = new mongoose.Schema({
+//   from: String,
+//   body: String,
+//   timestamp: Date,
+// });
 
-const MessageModel =
-  mongoose.models.Message || mongoose.model("Message", MessageSchema);
+// const MessageModel =
+//   mongoose.models.Message || mongoose.model("Message", MessageSchema);
 
-export default MessageModel;
+// export default MessageModel;
